edb713d8764eddd4d30ce1b8f2e7846d5f764a36
Elle Majors <hellolovely@ellemajors.com>
2023-01-31 18:59:45 -0700

    test: name and location not handlebars?

--- spec/models/salon_spec.rb
+++ spec/models/salon_spec.rb
@@ -8,6 +8,18 @@ RSpec.describe Salon, type: :model do
 
         expect(salon.handlebars?).to be true
       end
+
+      it 'returns true when the city is handlebars' do
+        salon = Salon.create!(name: 'salida', city: 'handlebars')
+
+        expect(salon.handlebars?).to be true
+      end
+
+      it 'returns false when the name or city is not handlebars' do
+        salon = Salon.create!(name: 'kid kutz', city: 'abilene')
+
+        expect(salon.handlebars?).to be false
+      end
     end
   end
 end
