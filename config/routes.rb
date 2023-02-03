a80aae69e8d42627b52163d2d6594dfe2354b01c
Elle Majors <hellolovely@ellemajors.com>
2023-02-02 17:13:02 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
 
   get '/salons', to: 'salons#index'
-  get '/salons/new', to: 'salons#new'
-  post '/salons', to: 'salons#create'
-  # config/routes.rb
-
   get '/salons/:id', to: "salons#show"
-  get '/salons/:id/edit', to: 'salons#edit'
-  patch '/salons/:id', to: 'salons#update'
-  delete '/salons/:id', to: 'salons#destroy'
+
+  # get '/salons/new', to: 'salons#new'
+  # post '/salons', to: 'salons#create'
+  # get '/salons/:id/edit', to: 'salons#edit'
+  # patch '/salons/:id', to: 'salons#update'
+  # delete '/salons/:id', to: 'salons#destroy'
 end
