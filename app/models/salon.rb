d440004520deb6326b5b7d102470e04437de641a
Elle Majors <hellolovely@ellemajors.com>
2023-02-03 16:20:35 -0700

    Add salon model

--- app/models/salon.rb
+++ app/models/salon.rb
@@ -1,13 +1,12 @@
-# app/models/salon.rb
-
 class Salon < ApplicationRecord
-  def handlebars?
-    if name.downcase == 'handlebars' || name.downcase.include?('handlebars')
-      return true
-    elsif city.downcase == 'handlebars'
-      return true
-    else
-      return false  
-    end
-  end
+  has_many :workstations
+  # def handlebars?
+  #   if name.downcase == 'handlebars' || name.downcase.include?('handlebars')
+  #     return true
+  #   elsif city.downcase == 'handlebars'
+  #     return true
+  #   else
+  #     return false  
+  #   end
+  # end
 end
