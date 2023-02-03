3bb3beca6f950328180947e33cbfe76dd5083538
Elle Majors <hellolovely@ellemajors.com>
2023-02-03 16:35:34 -0700

    Add s to workstations

--- spec/features/salons/show_spec.rb
+++ spec/features/salons/show_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 RSpec.describe 'the salons show page' do
   it 'shows the salons in the system' do
     salon = Salon.create!(name: 'ELLE', city: 'Denver', stars: 5, requires_insurance: true )
+    workstation = salon.workstations.create!(name: 'hour_workstation', chair_count: 3, available: true )
     visit "/salons/#{salon.id}"
+
+    expect(page).to have_content(salon.name)
   end
 
-#   As a visitor
-# When I visit '/parents/:id'
-# Then I see the parent with that id including the parent's attributes
-# (data from each column that is on the parent table)
+  it 'displays the salon with that id and salons attributes' do
+    salon = Salon.create!(name: 'ELLE', city: 'Denver', stars: 5, requires_insurance: true )
+    visit "/salons/:id#{salon.id}"
+    # save_and_open_page
+    expect(page).to have_content(salon.name)
+  end
 end
