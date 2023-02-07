f4a287557dbd49d3c41b0df644308aa9ed24d1f3
Elle Majors <hellolovely@ellemajors.com>
2023-02-07 16:38:53 -0700

    test: displays workstations with true boolean

--- spec/models/workstation_spec.rb
+++ spec/models/workstation_spec.rb
@@ -3,3 +3,11 @@ require 'rails_helper'
 RSpec.describe Workstation, type: :model do
   it {should belong_to :salon }
 end
+# this test belongs here but doesn't work here?
+# describe 'User Story 15' do 
+#   it 'displays workstations where the boolean column is `true`' do
+#     visit "/workstations"
+    
+#     expect(Workstation.available?).to eq([@hour_workstation, @half_day_workstation])
+#   end
+end   
